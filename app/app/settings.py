58ade0f8e602b50c91648482880bf3761f42b9c5
Joseph Boscan <joeypc.py@gmail.com>
2022-06-14 01:15:22 -0400

    Creating test for models and admin, and creating models and admin customazation

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,9 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-6l-+nq9ap!+ye^9#ii-+v79zm-(e6cu-nwgh8jllm+)sdw(7!9"
-)
+SECRET_KEY = "django-insecure-6l-+nq9ap!+ye^9#ii-+v79zm-(e6cu-nwgh8jllm+)sdw(7!9"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -130,3 +128,5 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "core.User"
--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,46 @@
+"""
+Database models.
+"""
 from django.db import models  # noqa
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager,
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    def create_user(self, email, password=None, **extra_kwargs):
+        """Create, save and return a new user."""
+        if not email:
+            raise ValueError("User must have an email address")
+
+        user = self.model(email=self.normalize_email(email), **extra_kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create, save  and return a new superuser."""
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
